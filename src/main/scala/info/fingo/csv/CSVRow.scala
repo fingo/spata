96120059971c78a7ed449b9f664b55742f93583b
Robert Marek <robert.marek@fingo.pl>
2019-04-05 22:08:10 +0200

    Fixed formatting

--- src/main/scala/info/fingo/csv/CSVRow.scala
+++ src/main/scala/info/fingo/csv/CSVRow.scala
@@ -1,9 +1,9 @@
 package info.fingo.csv
 
-class CSVRow(row: IndexedSeq[String], lineNum: Int, rowNum: Int)(implicit header: Map[String,Int]) {
+class CSVRow(row: IndexedSeq[String], lineNum: Int, rowNum: Int)(implicit header: Map[String, Int]) {
 
   if(row.length != header.size)
-    throw new CSVException("Bad format: wrong number of values","valuesNumber",lineNum,rowNum)
+    throw new CSVException("Bad format: wrong number of values","valuesNumber", lineNum, rowNum)
 
   def getString(key: String): String = {
     val pos = header(key)

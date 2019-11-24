8707e77640876ac781855d3a1c3a1160806c55b8
Robert Marek <robert.marek@fingo.pl>
2019-11-24 22:06:03 +0100

    Parsers for basic types added

--- src/main/scala/info/fingo/spata/StringParser.scala
+++ src/main/scala/info/fingo/spata/StringParser.scala
@@ -1,27 +1,44 @@
 package info.fingo.spata
 
-import java.text.DecimalFormat
-import java.time.LocalDate
+import java.text.{DecimalFormat, NumberFormat, ParseException}
+import java.time.{LocalDate, LocalDateTime, LocalTime}
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 import scala.util.Try
 
-trait StringParser[A] {
-  type FmtType
+trait SimpleStringParser[A] {
   def parse(str: String): Option[A]
+}
+
+trait StringParser[A] extends SimpleStringParser[A] {
+  type FmtType
   def parse(str: String, fmt: FmtType): Option [A]
 }
 
 object StringParser {
   type Aux[A,B] = StringParser[A] {type FmtType = B}
 
-  def parse[A](str: String)(implicit parser: StringParser[A]): Option[A] = parser.parse(str)
+  def parse[A](str: String)(implicit parser: SimpleStringParser[A]): Option[A] = parser.parse(str)
   def parse[A,B](str: String, fmt: B)(implicit parser: Aux[A,B]): Option[A] = parser.parse(str, fmt)
 
-  implicit val stringParser: StringParser[String] = new StringParser[String] {
-    override def parse(str: String): Option[String] = if(str.isEmpty) None else Some(str)
-    override def parse(str: String, fmt: FmtType): Option[String] = parse(str)
-  }
+  implicit val stringParser: SimpleStringParser[String] = (str: String) => if(str.isEmpty) None else Some(str)
+
+  implicit val intParser: SimpleStringParser[Int] = (str: String) => Try(str.toInt).toOption
+
+  implicit val longParser: StringParser[Long] {type FmtType = NumberFormat} =
+    new StringParser[Long] {
+      override type FmtType = NumberFormat
+      override def parse(str: String): Option[Long] = Try(str.toLong).toOption
+      override def parse(str: String, fmt: FmtType): Option[Long] = Try(fmt.parse(str).longValue()).toOption
+    }
+
+  implicit val doubleParser: StringParser[Double] {type FmtType = DecimalFormat} =
+    new StringParser[Double] {
+      override type FmtType = DecimalFormat
+      override def parse(str: String): Option[Double] = Try(str.toDouble).toOption
+      override def parse(str: String, fmt: FmtType): Option[Double] = Try(fmt.parse(str).doubleValue()).toOption
+    }
 
   implicit val bigDecimalParser: StringParser[BigDecimal] {type FmtType = DecimalFormat} =
     new StringParser[BigDecimal] {
@@ -39,4 +56,46 @@ object StringParser {
       override def parse(str: String): Option[LocalDate] = Try(LocalDate.parse(str)).toOption
       override def parse(str: String, fmt: FmtType): Option[LocalDate] = Try(LocalDate.parse(str, fmt)).toOption
     }
+
+  implicit val localTimeParser: StringParser[LocalTime] {type FmtType = DateTimeFormatter} =
+    new StringParser[LocalTime] {
+      override type FmtType = DateTimeFormatter
+      override def parse(str: String): Option[LocalTime] = Try(LocalTime.parse(str)).toOption
+      override def parse(str: String, fmt: FmtType): Option[LocalTime] = Try(LocalTime.parse(str, fmt)).toOption
+    }
+
+  implicit val localDateTimeParser: StringParser[LocalDateTime] {type FmtType = DateTimeFormatter} =
+    new StringParser[LocalDateTime] {
+      override type FmtType = DateTimeFormatter
+      override def parse(str: String): Option[LocalDateTime] = Try(LocalDateTime.parse(str)).toOption
+      override def parse(str: String, fmt: FmtType): Option[LocalDateTime] = Try(LocalDateTime.parse(str, fmt)).toOption
+    }
+
+  implicit val booleanParser: StringParser[Boolean] {type FmtType = BooleanFormatter} =
+    new StringParser[Boolean] {
+      override type FmtType = BooleanFormatter
+      override def parse(str: String): Option[Boolean] = Try(BooleanFormatter.default.parse(str)).toOption
+      override def parse(str: String, fmt: FmtType): Option[Boolean] = Try(fmt.parse(str)).toOption
+    }
+}
+
+class BooleanFormatter(tt: String, ft: String, locale: Locale) {
+
+  val trueTerm: String = tt.toLowerCase(locale)
+  val falseTerm: String = ft.toLowerCase(locale)
+
+  def this(trueTerm: String, falseTerm: String) = this(trueTerm, falseTerm, Locale.getDefault())
+
+  def format(value: Boolean): String = if(value) trueTerm else falseTerm
+  def parse(string: String): Boolean = string.strip().toLowerCase(locale) match {
+    case `trueTerm` => true
+    case `falseTerm` => false
+    case _ => throw new ParseException(s"Cannot parse $string as boolean", 0)
+  }
+}
+
+object BooleanFormatter {
+  def apply(tt: String, ft: String, locale: Locale): BooleanFormatter = new BooleanFormatter(tt, ft, locale)
+  def apply(tt: String, ft: String): BooleanFormatter = new BooleanFormatter(tt, ft)
+  val default: BooleanFormatter = apply(true.toString, false.toString)
 }
--- src/test/scala/info/fingo/spata/StringParserTS.scala
+++ src/test/scala/info/fingo/spata/StringParserTS.scala
@@ -1,7 +1,7 @@
 package info.fingo.spata
 
 import java.text.{DecimalFormat, NumberFormat}
-import java.time.LocalDate
+import java.time.{LocalDate, LocalDateTime, LocalTime}
 import java.time.format.{DateTimeFormatter, FormatStyle}
 import java.util.Locale
 
@@ -12,30 +12,87 @@ class StringParserTS extends FunSuite with TableDrivenPropertyChecks {
 
   import StringParser._
   val locale = new Locale("pl", "PL")
+  val nbsp = '\u00A0'
 
-  test("StringParser should parse correctly strings") {
+  test("StringParser should correctly parse strings") {
     forAll(strings) { (_: String, str: String, string: Option[String]) =>
       assert(parse[String](str) == string)
     }
   }
 
-  test("StringParser should parse correctly big decimals") {
-    forAll(bigDecimals) { (_: String, str: String, decimal: Option[BigDecimal], fmt: Option[DecimalFormat]) =>
+  test("StringParser should correctly parse ints") {
+    forAll(ints) { (_: String, str: String, int: Option[Int]) =>
+      assert(parse[Int](str) == int)
+    }
+  }
+
+  test("StringParser should correctly parse longs") {
+    forAll(longs) { (_: String, str: String, long: Option[Long], fmt: Option[NumberFormat]) =>
       val result = fmt match {
         case Some(f) => parse(str,f)
+        case _ => parse[Long](str)
+      }
+      assert(result == long)
+    }
+  }
+
+  test("StringParser should correctly parse doubles") {
+    forAll(doubles) { (_: String, str: String, double: Option[Double], fmt: Option[DecimalFormat]) =>
+      val result = fmt match {
+        case Some(f) => parse[Double,DecimalFormat](str,f)
+        case _ => parse[Double](str)
+      }
+      assert(result == double)
+    }
+  }
+
+  test("StringParser should correctly parse big decimals") {
+    forAll(decimals) { (_: String, str: String, decimal: Option[BigDecimal], fmt: Option[DecimalFormat]) =>
+      val result = fmt match {
+        case Some(f) => parse[BigDecimal,DecimalFormat](str,f)
         case _ => parse[BigDecimal](str)
       }
       assert(result == decimal)
     }
   }
 
-  test("StringParser should parse correctly local dates") {
+  test("StringParser should correctly parse local dates") {
     forAll(localDates) { (_: String, str: String, date: Option[LocalDate], fmt: Option[DateTimeFormatter]) =>
       val result = fmt match {
-        case Some(f) => parse(str,f)
+        case Some(f) => parse[LocalDate,DateTimeFormatter](str,f)
         case _ => parse[LocalDate](str)
       }
       assert(result == date)
+    }
+  }
+
+  test("StringParser should correctly parse local times") {
+    forAll(localTimes) { (_: String, str: String, time: Option[LocalTime], fmt: Option[DateTimeFormatter]) =>
+      val result = fmt match {
+        case Some(f) => parse[LocalTime,DateTimeFormatter](str,f)
+        case _ => parse[LocalTime](str)
+      }
+      assert(result == time)
+    }
+  }
+
+  test("StringParser should correctly parse local date-times") {
+    forAll(localDateTimes) { (_: String, str: String, dateTime: Option[LocalDateTime], fmt: Option[DateTimeFormatter]) =>
+      val result = fmt match {
+        case Some(f) => parse[LocalDateTime,DateTimeFormatter](str,f)
+        case _ => parse[LocalDateTime](str)
+      }
+      assert(result == dateTime)
+    }
+  }
+
+  test("StringParser should correctly parse booleans") {
+    forAll(booleans) { (_: String, str: String, boolean: Option[Boolean], fmt: Option[BooleanFormatter]) =>
+      val result = fmt match {
+        case Some(f) => parse(str,f)
+        case _ => parse[Boolean](str)
+      }
+      assert(result == boolean)
     }
   }
 
@@ -45,18 +102,65 @@ class StringParserTS extends FunSuite with TableDrivenPropertyChecks {
     ("empty", "", None)
   )
 
-  val bigDecimals = Table(
+  val ints = Table(
+    ("testCase","str","int"),
+    ("basic", "123456789", Some(123456789)),
+    ("negative", "-123456789", Some(-123456789)),
+    ("empty", "", None)
+  )
+
+  val longs = Table(
+    ("testCase","str","long","format"),
+    ("basic", "123456789", Some(123456789L), None),
+    ("locale", s"-123${nbsp}456${nbsp}789", Some(-123456789L), Some(NumberFormat.getInstance(locale))),
+    ("empty", "", None, None)
+  )
+
+  val doubles = Table(
+    ("testCase","str","double","format"),
+    ("basic", "123456.789", Some(123456.789), None),
+    ("integral", "123456789", Some(123456789.0), None),
+    ("locale", s"-123${nbsp}456,789", Some(-123456.789), Some(NumberFormat.getInstance(locale).asInstanceOf[DecimalFormat])),
+    ("empty", "", None, None)
+  )
+
+  val decimals = Table(
     ("testCase","str","decimal","format"),
-    ("basic", "123.45", Some(BigDecimal(123.45)), None),
-    ("locale", "123,45", Some(BigDecimal(123.45)), Some(NumberFormat.getInstance(locale).asInstanceOf[DecimalFormat])),
+    ("basic", "123456.789", Some(BigDecimal(123456.789)), None),
+    ("integral", "123456789", Some(BigDecimal(123456789)), None),
+    ("locale", s"-123${nbsp}456,789", Some(BigDecimal(-123456.789)), Some(NumberFormat.getInstance(locale).asInstanceOf[DecimalFormat])),
     ("empty", "", None, None)
   )
 
   val localDates = Table(
     ("testCase","str","date","format"),
-    ("basic", "2020-02-02", Some(LocalDate.of(2020,2,2)), None),
-    ("locale", "02.02.2020", Some(LocalDate.of(2020,2,2)), Some(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(locale))),
-    ("formatted", "02/02/2020", Some(LocalDate.of(2020,2,2)), Some(DateTimeFormatter.ofPattern("dd/MM/yyyy"))),
+    ("basic", "2020-02-29", Some(LocalDate.of(2020,2,29)), None),
+    ("locale", "29.02.2020", Some(LocalDate.of(2020,2,29)), Some(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(locale))),
+    ("formatted", "29/02/2020", Some(LocalDate.of(2020,2,29)), Some(DateTimeFormatter.ofPattern("dd/MM/yyyy"))),
     ("empty", "", None, None)
   )
-}
+
+  val localTimes = Table(
+    ("testCase","str","time","format"),
+    ("basic", "12:34:56", Some(LocalTime.of(12,34,56)), None),
+    ("milis", "12:34:56.789", Some(LocalTime.of(12,34,56, 789_000_000)), None),
+    ("locale", "12:34", Some(LocalTime.of(12,34)), Some(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withLocale(locale))),
+    ("formatted", "12:34 PM", Some(LocalTime.of(12,34)), Some(DateTimeFormatter.ofPattern("hh:mm a"))),
+    ("empty", "", None, None)
+  )
+
+  val localDateTimes = Table(
+    ("testCase","str","datetime","format"),
+    ("basic", "2020-02-29T12:34:56", Some(LocalDateTime.of(2020,2,29,12,34,56)), None),
+    ("locale", "29.02.2020, 12:34", Some(LocalDateTime.of(2020,2,29,12,34)), Some(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).withLocale(locale))),
+    ("formatted", "29/02/2020 12:34 PM", Some(LocalDateTime.of(2020,2,29,12,34)), Some(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a"))),
+    ("empty", "", None, None)
+  )
+
+  val booleans = Table(
+    ("testCase","str","boolean","format"),
+    ("basic", "true", Some(true), None),
+    ("locale", "FAŁSZ", Some(false), Some(BooleanFormatter("prawda", "fałsz", locale))),
+    ("formatted", "y", Some(true), Some(BooleanFormatter("y", "n"))),
+    ("empty", "", None, None)
+  )}

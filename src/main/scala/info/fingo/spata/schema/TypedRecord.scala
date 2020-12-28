108404b22d706cbd7243a2db6d4f8ff6d1acfe04
Robert Marek <robert.marek@fingo.info>
2020-12-28 20:20:44 +0100

    Add tests for typed record

--- src/main/scala/info/fingo/spata/schema/TypedRecord.scala
+++ src/main/scala/info/fingo/spata/schema/TypedRecord.scala
@@ -11,13 +11,14 @@ import shapeless.ops.record.Selector
 import shapeless.tag.@@
 import info.fingo.spata.schema.TypedRecord.ToProduct
 
-class TypedRecord[+L <: HList](val data: L, val lineNum: Int, val rowNum: Int) {
+class TypedRecord[+L <: HList] private[schema] (val data: L, val lineNum: Int, val rowNum: Int) {
   def apply[M >: L <: HList](key: StrSng)(implicit selector: Selector[M, key.type]): selector.Out = selector(data)
   def to[P <: Product]: ToProduct[P, L] = new ToProduct[P, L](data)
 }
 
 private[schema] object TypedRecord {
-  def apply[L <: HList](data: L, lineNum: Int, rowNum: Int): TypedRecord[L] = new TypedRecord[L](data, lineNum, rowNum)
+  def apply[L <: HList](data: L, lineNum: Int, rowNum: Int): TypedRecord[L] =
+    new TypedRecord[L](data, lineNum, rowNum)
 
   class ToProduct[P <: Product, +L <: HList](data: L) {
     def apply[M >: L <: HList, R <: HList]()(implicit lg: LabelledGeneric.Aux[P, R], tr: ToRepr[M, R]): P =

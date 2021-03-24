09338ac09dc423e65c59cae8177287d5009f6f43
Robert Marek <robert.marek@fingo.info>
2021-03-24 19:36:40 +0100

    Add Scaladoc to RecordFromHList

--- src/main/scala/info/fingo/spata/converter/RecordFromHList.scala
+++ src/main/scala/info/fingo/spata/converter/RecordFromHList.scala
@@ -10,15 +10,38 @@ import info.fingo.spata.text.StringRenderer
 import shapeless.labelled.FieldType
 import shapeless.{::, HList, HNil, Lazy, Witness}
 
+/** Converter from specific [[shapeless.HList]] to Record through RecordBuilder.
+  *
+  * This trait defines behavior to be implemented by concrete, implicit converters.
+  *
+  * @tparam L type of source `HList`
+  */
 trait RecordFromHList[L <: HList] {
 
+  /** Converts [[shapeless.HList]] to Record through RecordBuilder.
+    *
+    * @param hList the source `HList` to be converted
+    * @return
+    */
   def apply(hList: L): RecordBuilder
 }
 
+/** Implicits to convert [[shapeless.HNil]] and [[shapeless.::]] to RecordBuilder. */
 object RecordFromHList {
 
+  /** Converter from [[shapeless.HNil]] */
   implicit val fromHNil: RecordFromHList[HNil] = _ => new RecordBuilder()
 
+  /** Converter from [[shapeless.::]].
+    *
+    * @param witness carrier of field information (name as singleton type)
+    * @param renderer renderer from typed value to string
+    * @param rFromHL converter for [[shapeless.HList]] tail
+    * @tparam K singleton type representing field name being converted from cons
+    * @tparam V type of single `HList` element which value is being rendered
+    * @tparam T type of `HList` tail
+    * @return converter from `HList` to RecordBuilder
+    */
   implicit def fromHCons[K <: Symbol, V, T <: HList](
     implicit witness: Witness.Aux[K],
     renderer: StringRenderer[V],

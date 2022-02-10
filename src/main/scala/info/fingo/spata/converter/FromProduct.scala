4752cdab75ac5eb11c921de9522e8724a72a38f3
Robert Marek <robert.marek@fingo.info>
2022-02-10 19:53:47 +0100

    Implement Product to Record conversion

--- src/main/scala/info/fingo/spata/converter/FromProduct.scala
+++ src/main/scala/info/fingo/spata/converter/FromProduct.scala
@@ -5,18 +5,29 @@
  */
 package info.fingo.spata.converter
 
-import info.fingo.spata.{Decoded, Record}
+import info.fingo.spata.text.StringRenderer
+import info.fingo.spata.{Decoded, Header, Record}
+
+import scala.deriving.Mirror
 
 trait FromProduct[P <: Product]:
   def encode(p: P): Record
 
 trait FromTuple[-T <: Tuple]:
-  def encode(t: T): Record
+  def encode(t: T): Record = Record.fromValues(encodeRaw(t)*)
+  def encodeRaw(t: T): List[String]
 
 object FromTuple:
-  // FIXME: implement
-  given fromTuple[T <: Tuple]: FromTuple[T] = ???
+  given fromEmpty: FromTuple[EmptyTuple] with
+    def encodeRaw(et: EmptyTuple): List[String] = Nil
+  given fromCons[H: StringRenderer, T <: Tuple: FromTuple]: FromTuple[H *: T] with
+    def encodeRaw(t: H *: T): List[String] = summon[StringRenderer[H]](t.head) :: summon[FromTuple[T]].encodeRaw(t.tail)
 
 object FromProduct:
-  // FIXME: implement
-  given fromProduct[P <: Product]: FromProduct[P] = ???
+  inline given fromProduct[P <: Product](using
+    m: Mirror.ProductOf[P],
+    ft: FromTuple[m.MirroredElemTypes]
+  ): FromProduct[P] = (p: P) =>
+    val values = ft.encodeRaw(Tuple.fromProductTyped(p))
+    val labels = p.productElementNames.toList
+    Record(values*)(Header(labels*))

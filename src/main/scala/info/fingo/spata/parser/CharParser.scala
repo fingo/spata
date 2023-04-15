473614d2a65ff75f24c27854e3a6a0faad83aa5b
Robert Marek <robert.marek@fingo.info>
2023-04-15 22:00:25 +0200

    Adapt syntax of parser package to Scala 3

--- src/main/scala/info/fingo/spata/parser/CharParser.scala
+++ src/main/scala/info/fingo/spata/parser/CharParser.scala
@@ -6,50 +6,45 @@
 package info.fingo.spata.parser
 
 import fs2.{Chunk, Pipe, Pull, Stream}
-import info.fingo.spata.error.ParsingErrorCode._
+import info.fingo.spata.error.ParsingErrorCode.*
 
 /* A finite-state transducer to converter plain source characters into context-dependent symbols,
  * taking into consideration special meaning of some characters (e.g. separators), quoting and escaping.
  */
-final private[spata] class CharParser[F[_]](fieldDelimiter: Char, recordDelimiter: Char, quote: Char, trim: Boolean) {
-  import CharParser._
-  import CharParser.CharPosition._
+final private[spata] class CharParser[F[_]](fieldDelimiter: Char, recordDelimiter: Char, quote: Char, trim: Boolean):
+
+  import CharParser.*
+  import CharParser.CharPosition.*
 
   /* Transforms plain characters into context-dependent symbols by providing FS2 pipe. */
   def toCharResults: Pipe[F, Char, CharResult] = toCharResults(CharState(Left(STX), Start))
 
   /* Parse characters and covert them to CharResults based on previous state. Stop on first failure. */
-  private def toCharResults(state: CharState): Pipe[F, Char, CharResult] = {
-
+  private def toCharResults(state: CharState): Pipe[F, Char, CharResult] =
     def loop(chars: Stream[F, Char], state: CharState): Pull[F, CharResult, Unit] =
       chars.pull.uncons.flatMap {
         case Some((h, t)) =>
           val (nextState, resultChunk) = parseChunk(h, state)
-          nextState match {
+          nextState match
             case cs: CharState => Pull.output(resultChunk) >> loop(t, cs)
             case cf: CharFailure => Pull.output(dropFailures(resultChunk)) >> Pull.output1(cf) >> Pull.done
-          }
         case None => Pull.output1(endOfStream(state)) >> Pull.done
       }
-
     chars => loop(chars, state).stream
-  }
 
   /* Parse chunk of characters into chunk of CharResults. */
   private def parseChunk(chunk: Chunk[Char], state: CharState) =
-    chunk.mapAccumulate(state: CharResult) { (s, c) =>
-      val nextState = s match {
+    chunk.mapAccumulate(state: CharResult)((s, c) =>
+      val nextState = s match
         case cs: CharState => parseChar(c, cs)
         case cf: CharFailure => cf
-      }
       (nextState, nextState)
-    }
+    )
 
   private def endOfStream(state: CharState): CharResult =
-    state.position match {
+    state.position match
       case Quoted => CharFailure(UnmatchedQuotation)
       case _ => CharState(Left(ETX), FinishedRecord)
-    }
 
   /* Keep only successful parsing results to output failure state once only. */
   private def dropFailures(chunk: Chunk[CharResult]) = chunk.filter {
@@ -57,15 +52,14 @@ final private[spata] class CharParser[F[_]](fieldDelimiter: Char, recordDelimite
     case _ => false
   }
 
-  @inline
-  private def isDelimiter(c: Char): Boolean = c == fieldDelimiter || c == recordDelimiter
+  private inline def isDelimiter(c: Char): Boolean = c == fieldDelimiter || c == recordDelimiter
 
-  @inline
-  private def isWhitespace(c: Char): Boolean = trim && c.isWhitespace // without trimming, whitespace is a regular char
+  private inline def isWhitespace(c: Char): Boolean =
+    trim && c.isWhitespace // without trimming, whitespace is a regular char
 
   /* Core translating function - state transitions. */
   private def parseChar(char: Char, state: CharState): CharResult =
-    char match {
+    char match
       case `quote` if state.atBeginning => CharState(Left(char), Quoted)
       case `quote` if state.position == Quoted => CharState(Left(char), Escape)
       case `quote` if state.position == Escape => CharState(Right(quote), Quoted)
@@ -82,21 +76,20 @@ final private[spata] class CharParser[F[_]](fieldDelimiter: Char, recordDelimite
       case c if state.atBeginning => CharState(Right(c), Regular)
       case c if state.position == Trailing => CharState(Right(c), Regular)
       case c => CharState(Right(c), state.position)
-    }
 
   val newLineDelimiter: Option[Char] = List(recordDelimiter, fieldDelimiter, quote).find(_ == LF)
-}
 
-private[spata] object CharParser {
+private[spata] object CharParser:
+
   val LF: Char = 0x0A.toChar
   val CR: Char = 0x0D.toChar
   val STX: Char = 0x02.toChar
   val ETX: Char = 0x03.toChar
 
-  object CharPosition extends Enumeration {
+  object CharPosition extends Enumeration:
     type CharPosition = Value
     val Start, Regular, Quoted, Trailing, Escape, End, FinishedField, FinishedRecord = Value
-  }
+
   import CharPosition._
 
   sealed trait CharResult
@@ -107,12 +100,10 @@ private[spata] object CharParser {
    * If character retains its direct meaning it is represented as Right.
    * If it is interpreted as a special character or is skipped, it is represented as Left.
    */
-  case class CharState(char: Either[Char, Char], position: CharPosition) extends CharResult {
+  case class CharState(char: Either[Char, Char], position: CharPosition) extends CharResult:
     def isSimple: Boolean = position == Regular || position == Trailing
     def finished: Boolean = position == FinishedField || position == FinishedRecord
     def atBoundary: Boolean = position == Start || position == End
     def atBeginning: Boolean = position == Start || finished
     def isNewLine: Boolean = char.fold(_ == LF, _ == LF)
     def hasChar: Boolean = char.isRight
-  }
-}
